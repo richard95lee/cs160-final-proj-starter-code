94cbcd0c55a5c788063eecab9420aa458af4af1d
Richard Lee <richard95lee@berkeley.edu>
2021-07-16 01:03:54 +0900

    1

--- coloring/urls.py
+++ coloring/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('demo', views.demo, name='demo'),
-    path('new_interaction', views.new_interaction, name='new_interaction')
+    path('new_interaction', views.new_interaction, name='new_interaction'),
+    path('main', views.main, name='main'),
+    path('color1', views.color1, name='color1'),
+    path('color2', views.color2, name='color2'),
+    path('color3', views.color3, name='color3')
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -8,3 +8,15 @@ def demo(request):
 
 def new_interaction(request):
     return render(request, 'coloring/new_interaction.html')
+
+def main(request):
+    return render(request, 'coloring/main.html')
+
+def color1(request):
+    return render(request, 'coloring/color1.html')
+
+def color2(request):
+    return render(request, 'coloring/color2.html')
+
+def color3(request):
+    return render(request, 'coloring/color3.html')
