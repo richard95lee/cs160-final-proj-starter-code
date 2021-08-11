f896d0ec4dcfa01b453b20b4ec869a7f4f108525
Richard Lee <richard95lee@berkeley.edu>
2021-08-12 01:13:58 +0900

    added view page

--- coloring/urls.py
+++ coloring/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('index', views.index, name='index'),
     path('bedroom', views.bedroom, name='bedroom'),
+    path('viewdesign', views.viewdesign, name='viewdesign'),
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'coloring/index.html');
+    return render(request, 'coloring/index.html')
 
 def bedroom(request):
-    return render(request, 'coloring/bedroom.html');
+    return render(request, 'coloring/bedroom.html')
+
+def viewdesign(request):
+    return render(request, 'coloring/viewdesign.html')
